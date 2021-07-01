d80ab1c080c74a90210e6af592e8096ff480353d
raghavm8 <raghav.mittal1109@gmail.com>
2021-07-01 13:47:47 +0530

    Code Added

---
+++ CommandDemo/ViewModels/MessageViewModel.cs
@@ -0,0 +1,60 @@
+﻿using CommandDemo.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CommandDemo.ViewModels
+{
+    public class MessageViewModel
+    {
+        public ObservableCollection<string> messages { get; private set; }
+
+        public RelayCommand MessageBoxCommand { get; private set; }
+        public RelayCommand ConsoleLogCommand { get; private set; }
+
+        public MessageViewModel()
+        {
+            messages = new ObservableCollection<string>()
+            {
+                "Message Box",
+                "Hello world",
+                "Hello Laptop",
+                "Console Log"
+            };
+
+            MessageBoxCommand = new RelayCommand(DisplayMessageBox, MessageBoxCanUse);
+            ConsoleLogCommand = new RelayCommand(DisplayConsoleBox, ConsoleCanUse);
+        }
+
+        public void DisplayMessageBox(object message)
+        {
+            MessageBox.Show((string)message);
+        }
+
+        public void DisplayConsoleBox(object message)
+        {
+            MessageBox.Show((string)message);
+        }
+
+        public bool MessageBoxCanUse(object message)
+        {
+            if ((string)message == "Console Log")
+                return false;
+
+            return true;
+        }
+
+        public bool ConsoleCanUse(object message)
+        {
+            if ((string)message == "Message Box")
+                return false;
+
+            return true;
+        }
+
+    }
+}
